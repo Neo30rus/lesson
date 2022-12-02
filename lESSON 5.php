8edf025b2a8508390b8d9f5e7c8326bb5554abb9
alevur <alevure@yandex.ru>
2022-12-02 20:30:27 +0400

    Initial commit

---
+++ lESSON 5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Рисунок по левому краю</title>
+     <link rel="stylesheet" href="l"
+</head>
+<body>
+
+<p><a href="lorem.html"><img src="/"
+                             width="225" height="225" alt="lorem"></a>
+    Lorem ipsum dolor sit amet...</p>
+</body>
+</html>
